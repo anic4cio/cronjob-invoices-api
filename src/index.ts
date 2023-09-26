0786754a2ffc095f1e57ea22bf97cf5cfd69b561
anic4cio <anicaciovinicius36@gmail.com>
2023-09-26 18:15:46 -0300

    build: exporting start

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,6 @@
 import { sendFileToSlack } from './services/slackSender'
-import { getAllAppointments } from './services/appointmentsGetter'
+import { makePDF } from './services/pdfGenerator'
 
 exports.start = async () => {
-  await sendAppointmentsToSlack()
-}
-
-const today = () => {
-  const date = new Date().toLocaleString('en-US', {
-    timeZone: 'America/Sao_Paulo'
-  })
-  return new Date(date).toLocaleDateString().replaceAll('/', '-')
-}
-
-const getAppointmentsDataBuffer = async () => {
-  const acuityAppointments = await getAllAppointments()
-  return Buffer.from(JSON.stringify(acuityAppointments))
-}
-
-const sendAppointmentsToSlack = async () => {
-  const message = today()
-  const file = await getAppointmentsDataBuffer()
-  await sendFileToSlack({ file, message })
+  await makePDF()
 }
